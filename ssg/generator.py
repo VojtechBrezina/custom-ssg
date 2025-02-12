d2df83c135f0e7ff94eb377eed84173941191771
Vojtech Brezina <vojta.brezina@gmail.com>
2025-02-12 12:57:17 +0100

    Add the usual viewport meta.

--- ssg/generator.py
+++ ssg/generator.py
@@ -99,7 +99,9 @@ class Generator:
                 f"""
                 <!DOCTYPE html>
                 <html lang={self.site_lang}>
-                    <head></head>
+                    <head>
+                        <meta name="viewport" content="width=device-width,initial-scale=1" />
+                    </head>
                     <body><main><article></article></main></body>
                 </html>
                 """,
